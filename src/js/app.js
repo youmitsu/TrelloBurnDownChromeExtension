bfc9db39626636cf002081b4b35183f171104d75
youmitsu <ymbullseye@gmail.com>
2018-09-06 18:25:45 +0900

    display menu with vuex

--- src/js/app.js
+++ src/js/app.js
@@ -1,38 +1,68 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from '../node_modules/vue/dist/vue.js';
+import Vuex from '../node_modules/vuex/dist/vuex.js';
+import graphMenu from './components/graphMenu.vue';
 Vue.use(Vuex);
 
 var store = new Vuex.Store({
   state: {
-    // todos: [],
-    // dones: []
+    loadState: {
+      loading: false,
+      status: "", // status: "" or "SUCCESS" or "FAILED"
+    },
+    selectedBoard: {
+      boardId: localStorage.getItem('boardId'),
+      boardName: localStorage.getItem('boardName')
+    },
+    boardItems: [],
+    graph: {
+      startDate: localStorage.getItem('startDate'),
+      endDate: localStorage.getItem('endDate'),
+      holidays: localStorage.getItem('holidays'),
+      data: null
+    }
   },
   getters: {
-    // todos(state) {
-    //   return state.todos;
-    // },
+    boardDefaultText: state => {
+      return state.selectedBoard.boardName || "ボードを選択";
+    },
+    boardList: state => {
+      return state.boardItems.length <= 0 ? ["ボードがありません"] : state.boardItems;
+    }
   },
   actions: {
-    // addTodo(context, todo) {
-    //   context.commit('ADD_TODO', todo.text);
-    // },
+    setSelectBoard({commit}, boardItem) {
+      commit('SET_SELECT_BOARD', boardItem);
+    },
+    setBoardItems({commit}, boardItems) {
+      commit('SET_BOARD_ITEMS', boardItems);
+    },
+    reload(context) {
+      location.reload();
+    }
   },
   mutations: {
-    // ADD_TODO(state, text) {
-    //   var todo = {
-    //     id: 0,
-    //     text: text
-    //   }
-    //   if (state.todos.length !== 0) {
-    //     todo.id = state.todos[state.todos.length - 1].id + 1;
-    //   }
-    //   state.todos.push(todo);
-    // },
+    SET_SELECT_BOARD(state, boardItem) {
+      state.selectedBoard = boardItem;
+    },
+    SET_START_DATE(state, startDate) {
+      state.graph.startDate = startDate;
+    },
+    SET_END_DATE(state, endDate) {
+      state.graph.endDate = endDate;
+    },
+    SET_HOLIDAYS(state, holidays) {
+      state.graph.holidays = holidays;
+    }
   }
 });
 
 new Vue({
   el: '#app',
   store,
-  components: {}
+  created: function() {
+    
+  },
+  components: {
+    "graph-menu": graphMenu
+  }
 })
---
+++ src/js/components/graphMenu.vue
@@ -0,0 +1,134 @@
+<template>
+<div class="ui secondary pointing inverted massive menu">
+  <div id="boardSelectArea" class="ui dropdown item popup">
+    <div class="text" v-text="boardDefaultText"></div>
+    <i class="dropdown icon"></i>
+    <div id="dropmenu" class="menu">
+      <div v-for="boardItem in boardList" @click="registerBoard(boardItem)" v-bind:class="{active: boardItem.isActive, selected: boardItem.isActive}" class="item" v-bind:value="boardItem.boardId" v-bind:data-value="boardItem.boardName">{{boardItem.boardName}}</div>
+    </div>
+  </div>
+  <div class="browse item">
+    <span class="ui dropdown" v-text="graph.startDate"></span> ~
+    <span class="ui dropdown" v-text="graph.endDate"></span>
+  </div>
+  <div class="ui flowing popup">
+    <div class="ui form">
+      <div id="startDate" class="field ui calendar required">
+        <label>StartDate</label>
+        <div class="ui left icon input">
+          <i class="calendar icon"></i>
+          <input id="start" type="text" placeholder="開始日" required v-model="startDate">
+        </div>
+      </div>
+      <div id="endDate" class="field ui calendar required">
+        <label>EndDate</label>
+        <div class="ui left icon input">
+          <i class="calendar icon"></i>
+          <input id="end" type="text" placeholder="終了日" required v-model="endDate" />
+        </div>
+      </div>
+      <div class="field">
+        <label>Holidays</label>
+        <div class="ui left icon input">
+          <i class="calendar icon"></i>
+          <input id="holidays" type="text" placeholder="休日" v-model="holidays"/>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="right menu">
+    <a class="item">
+        <i class="ui sync icon" @click="reload"></i>
+      </a>
+    <a class="item" href="./setting.html">
+        <i class="ui cog icon"></i>
+      </a>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  computed: {
+    boardDefaultText() {
+      return this.$store.getters.boardDefaultText;
+    },
+    boardList() {
+      return this.$store.getters.boardList;
+    },
+    graph() {
+      return this.$store.state.graph;
+    },
+    startDate: {
+      get() {
+        return this.$store.state.graph.startDate;
+      },
+      set(value) {
+        this.$store.commit('SET_START_DATE', value);
+      }
+    },
+    endDate: {
+      get() {
+        return this.$store.state.graph.endDate;
+      },
+      set(value) {
+        this.$store.commit('SET_END_DATE', value);
+      }
+    },
+    holidays: {
+      get() {
+        return this.$store.state.graph.holidays;
+      },
+      set(value) {
+        this.$store.commit('SET_HOLIDAYS', value);
+      }
+    }
+  },
+  methods: {
+    reload() {
+      this.$store.dispatch('reload');
+    },
+    registerBoard(boardItem) {
+      this.$store.commit('SET_SELECT_BOARD', boardItem);
+    }
+  },
+  mounted: function() {
+    console.log("child");
+    $('.ui.accordion').accordion();
+    $('.ui.dropdown').dropdown();
+    $('.menu .browse').popup({
+      hoverable: true,
+      position: 'bottom center',
+      on: 'click'
+    });
+    $('#startDate.ui.calendar').calendar({
+      type: 'date',
+      formatter: {
+        date: function(date) {
+          var day = ('0' + date.getDate()).slice(-2);
+          var month = ('0' + (date.getMonth() + 1)).slice(-2);
+          var year = date.getFullYear();
+          return year + '/' + month + '/' + day;
+        }
+      },
+      onChange: function(date, text, mode) {
+        this.$store.state.commit('SET_START_DATE', text);
+      }
+    });
+    $('#endDate.ui.calendar').calendar({
+      type: 'date',
+      formatter: {
+        date: function(date) {
+          var day = ('0' + date.getDate()).slice(-2);
+          var month = ('0' + (date.getMonth() + 1)).slice(-2);
+          var year = date.getFullYear();
+          return year + '/' + month + '/' + day;
+        }
+      },
+      onChange: function(date, text, mode) {
+        this.$store.state.commit('SET_END_DATE', text);
+      }
+    });
+  }
+}
+</script>
