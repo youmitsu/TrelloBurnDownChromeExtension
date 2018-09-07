c48b2eed67fb014fa52eb392a0488573c842d78f
youmitsu <ymbullseye@gmail.com>
2018-09-08 00:53:44 +0900

    displaying input form

---
+++ src/js/components/setting/formInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- <i class="ui notched circle loading icon" v-if="serverAuth.loadState == 'loading'"></i> -->
+    <i class="ui notched circle loading icon"></i>
+    <form class="ui large form">
+      <div class="field">
+      <!-- <div class="field" v-bind:class="{disabled: serverAuth.loadState == 'loading'}"> -->
+        <label>{{formType}}</label>
+        <div class="ui input">
+          <!-- <input id="url" type="text" v-bind:placeholder="type" v-model="serverAuth.baseUrl" @change="validateServer" required> -->
+          <input type="text" v-bind:placeholder="formType" required>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+<script>
+  export default {
+    props: ["formType"]
+  }
+</script>
--- src/js/components/settingBackend.vue
+++ src/js/components/settingBackend.vue
@@ -3,14 +3,16 @@
     <setting-title v-bind:title="title"></setting-title>
     <form-error v-bind:message="successMessage"></form-error>
     <form-success v-bind:message="errorMessage"></form-success>
+    <form-input v-bind:formType="formType"></form-input>
   </div>
 </template>
 <script>
   import settingTitle from './setting/title.vue';
   import formError from './setting/formError.vue';
   import formSuccess from './setting/formSuccess.vue';
+  import formInput from './setting/formInput.vue';
   export default {
-    props: ["title", "successMessage", "errorMessage"],
+    props: ["title", "successMessage", "errorMessage", "formType"],
     computed: {
       isLoadingError() {
         return this.$store.getters.setting.isLoadingError;
@@ -19,7 +21,8 @@
     components: {
       'setting-title': settingTitle,
       'form-error': formError,
-      'form-success': formSuccess
+      'form-success': formSuccess,
+      'form-input': formInput
     }
   }
 </script>
