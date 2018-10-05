0b0a313b9b4829130d34aac21d727fecd1e649e5
youmitsu <ymbullseye@gmail.com>
2018-10-06 01:36:28 +0900

    added datePicker

--- src/js/components/TheGraphMenus.vue
+++ src/js/components/TheGraphMenus.vue
@@ -1,5 +1,6 @@
 <template>
-<v-toolbar>
+<v-toolbar
+  height=70>
   <v-flex xs12 sm6 d-flex>
     <v-select
       v-model="selectedBoard"
@@ -8,50 +9,60 @@
       item-value="boardId"
       label="Selected Board"
       :loading="isLoading"
+      :disabled="isLoadingError"
       return-object
       ></v-select>
   </v-flex>
   <v-flex xs3 sm3 md3>
     <v-menu
-      ref="startMenu"
-      :close-on-content-click="false"
-      v-model="startMenu" :nudge-right="40"
-      :return-value.sync="startDate"
-      lazy
-      transition="scale-transition"
-      offset-y
-      full-width
-      min-width="290px">
-      <v-text-field slot="activator" v-model="startDate" label="StartDate" prepend-icon="event" readonly></v-text-field>
-      <v-date-picker v-model="startDate" no-title scrollable>
-        <v-spacer></v-spacer>
-        <v-btn flat color="primary" @click="startMenu = false">Cancel</v-btn>
-        <v-btn flat color="primary" @click="$refs.startMenu.save(startDate)">OK</v-btn>
-      </v-date-picker>
+        ref="startMenu"
+        :close-on-content-click="false"
+        v-model="startMenu"
+        :nudge-right="60"
+        :return-value.sync="startDate"
+        lazy
+        transition="scale-transition"
+        offset-y
+        full-width
+        min-width="290px"
+    >
+        <v-text-field
+          slot="activator"
+          v-model="startDate"
+          label="Start"
+          prepend-icon="event"
+          readonly
+        ></v-text-field>
+        <v-date-picker v-model="startDate" @input="$refs.startMenu.save(startDate)"></v-date-picker>
     </v-menu>
   </v-flex>
   <v-flex xs3 sm3 md3>
     <v-menu
-      ref="endMenu"
-      :close-on-content-click="false"
-      v-model="endMenu"
-      :nudge-right="40"
-      :return-value.sync="endDate"
-      lazy
-      transition="scale-transition"
-      offset-y
-      full-width
-      min-width="290px">
-      <v-text-field slot="activator" v-model="endDate" label="EndDate" prepend-icon="event" readonly></v-text-field>
-      <v-date-picker v-model="endDate" no-title scrollable>
-        <v-spacer></v-spacer>
-        <v-btn flat color="primary" @click="endMenu = false">Cancel</v-btn>
-        <v-btn flat color="primary" @click="$refs.endMenu.save(endDate)">OK</v-btn>
-      </v-date-picker>
+        ref="endMenu"
+        :close-on-content-click="false"
+        v-model="endMenu"
+        :nudge-right="60"
+        :return-value.sync="endDate"
+        lazy
+        transition="scale-transition"
+        offset-y
+        full-width
+        min-width="290px"
+    >
+        <v-text-field
+          slot="activator"
+          v-model="endDate"
+          label="End"
+          prepend-icon="event"
+          readonly
+        ></v-text-field>
+        <v-date-picker v-model="endDate" @input="$refs.endMenu.save(endDate)"></v-date-picker>
     </v-menu>
   </v-flex>
   <v-spacer></v-spacer>
-  <v-icon>sync</v-icon>
+  <v-flex xs1 sm1 md1>
+    <v-icon @click="reload">sync</v-icon>
+  </v-flex>
 </v-toolbar>
 </template>
 <script>
@@ -63,12 +74,6 @@ export default {
     }
   },
   computed: {
-    isInputedBoard() {
-      return this.$store.getters['graph/isInputedBoard'];
-    },
-    boardText() {
-      return this.$store.getters['graph/boardDefaultText'];
-    },
     boardList() {
       return this.$store.state.graph.boardItems;
     },
@@ -80,9 +85,6 @@ export default {
         this.$store.commit('graph/SET_SELECT_BOARD', value);
       }
     },
-    graph() {
-      return this.$store.state.graph.graph;
-    },
     isLoading() {
       return this.$store.state.graph.boardLoadState.loading;
     },
@@ -91,8 +93,7 @@ export default {
     },
     startDate: {
       get() {
-        // return this.$store.state.graph.graph.startDate;
-        new Date();
+        return this.$store.state.graph.graph.startDate;
       },
       set(value) {
         this.$store.commit('graph/SET_START_DATE', value);
@@ -100,8 +101,7 @@ export default {
     },
     endDate: {
       get() {
-        // return this.$store.state.graph.graph.endDate;
-        new Date();
+        return this.$store.state.graph.graph.endDate;
       },
       set(value) {
         this.$store.commit('graph/SET_END_DATE', value);
