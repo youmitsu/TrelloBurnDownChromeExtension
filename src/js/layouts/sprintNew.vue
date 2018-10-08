e16f912271501d97d5d93fab21326a1dd64d36ed
youmitsu <ymbullseye@gmail.com>
2018-10-09 00:59:07 +0900

    sprintNew back

--- src/js/layouts/sprintNew.vue
+++ src/js/layouts/sprintNew.vue
@@ -1,8 +1,28 @@
 <template>
 <div>
+  <v-toolbar
+    color="transparent"
+    scroll-off-screen
+    scroll-target="#scrolling-target"
+  >
+    <v-btn icon @click="back">
+      <v-icon>arrow_back</v-icon>
+    </v-btn>
+    <v-toolbar-title>New Sprint</v-toolbar-title>
+  </v-toolbar>
+  <v-container id="scrolling-target">
+  </v-container>
 </div>
 </template>
 <script>
 export default {
+  methods: {
+    back() {
+      this.$store.commit('SET_CURRENT_VIEW', {
+        isHome: true,
+        view: 'graph'
+      });
+    }
+  }
 }
 </script>
