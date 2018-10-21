c0caf8fa12541b5a419530f6225245999d0f704c
youmitsu <ymbullseye@gmail.com>
2018-10-21 17:24:16 +0900

    show_sprints

--- src/js/layouts/sprints.vue
+++ src/js/layouts/sprints.vue
@@ -8,34 +8,66 @@
     <v-toolbar-side-icon></v-toolbar-side-icon>
     <v-toolbar-title>Sprints</v-toolbar-title>
     <v-spacer></v-spacer>
+    <v-btn icon>
+      <v-icon @click="showSprintNew">add</v-icon>
+    </v-btn>
   </v-toolbar>
   <v-container id="scrolling-target">
-    <v-expansion-panel>
-      <v-expansion-panel-content
-        v-for="(item,i) in 5"
-        :key="i"
-      >
-        <div slot="header">Item</div>
-        <v-card>
-          <v-list two-line>
-            <template v-for="(itemm,i) in 5">
-              <v-list-tile
-                  :key="i"
-                  avatar
-                  @click=""
-                >
-                <v-list-tile-content>
-                  <v-list-tile-title></v-list-tile-title>
-                  <v-list-tile-sub-title></v-list-tile-sub-title>
-                </v-list-tile-content>
-              </v-list-tile>
-            </template>
-          </v-list>
-        </v-card>
-      </v-expansion-panel-content>
-    </v-expansion-panel>
+    <v-select
+      :items="boardList"
+      label="Board"
+      item-text="boardName"
+      item-value="boardId"
+      v-model="selectedBoard"
+      return-object
+    ></v-select>
+    <v-list two-line v-show="sprints.length > 0">
+      <template v-for="(item, i) in sprints">
+        <v-list-tile
+            :key="i"
+            avatar
+            @click=""
+          >
+          <v-list-tile-content>
+            <v-list-tile-title>{{item.name}}</v-list-tile-title>
+            <v-list-tile-sub-title>{{item.startDate}} ~ {{item.endDate}}</v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </template>
+    </v-list>
   </v-container>
 </div>
 </template>
 <script>
+export default {
+  data() {
+    return {
+      selectedBoard: null
+    }
+  },
+  mounted() {
+    this.$store.dispatch('graph/loadBoardList')
+      .then(() => {
+      });
+  },
+  computed: {
+    boardList() {
+      return this.$store.getters['graph/boardList'];
+    },
+    sprints() {
+      if(!this.selectedBoard) {
+        return [];
+      }
+      return this.$store.getters['graph/sprintsOfBoard'](this.selectedBoard.boardId);
+    }
+  },
+  methods: {
+    showSprintNew() {
+      this.$store.commit('SET_CURRENT_VIEW', {
+        view: 'sprintNew',
+        isHome: false
+      });
+    }
+  }
+}
 </script>
