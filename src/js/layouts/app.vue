65d50e7b85bcf436bf9f6f4264902d597c54d92d
youmitsu <ymbullseye@gmail.com>
2018-11-04 17:00:23 +0900

    show tutorial

--- src/js/layouts/app.vue
+++ src/js/layouts/app.vue
@@ -2,6 +2,7 @@
 <div>
   <home-view v-show="isHome"></home-view>
   <sprint-new v-show="isSprintNew"></sprint-new>
+  <tutorial-view v-show="shouldShowTutorialView"></tutorial-view>
 </div>
 </template>
 <script>
@@ -9,9 +10,11 @@ import * as DataStore from '../lib/dataStore.js';
 import viewType from '../common/viewType.js';
 import homeView from './home.vue';
 import sprintNew from './sprintNew.vue';
+import tutorialView from './tutorialView.vue';
 
 export default {
   created() {
+    this.$store.commit('SET_INITIAL_STATE');
     this.$store.commit('setting/SET_INITIAL_STATE');
     this.$store.commit('graph/SET_INITIAL_STATE');
   },
@@ -21,11 +24,16 @@ export default {
     },
     isSprintNew() {
       return !this.$store.state.current.isHome && this.$store.state.current.view == viewType.SPRINT_NEW.name;
+    },
+    shouldShowTutorialView() {
+      console.log(this.$store.getters.shouldShowTutorial);
+      return this.$store.getters.shouldShowTutorial;
     }
   },
   components: {
     "home-view": homeView,
-    "sprint-new": sprintNew
+    "sprint-new": sprintNew,
+    "tutorial-view": tutorialView
   }
 }
 </script>
